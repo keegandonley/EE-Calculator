46716a56c4c2ee3abbbc30a5edb782d555c4f6c6
Keegan Donley <keegan.donley@gmail.com>
2016-04-11 00:37:11 -0700

    Calculate resistor value based on color code (4-band)

--- EEHelp.py
+++ EEHelp.py
@@ -3,8 +3,22 @@ Calulates simple equations for use in basic electrical engineering
 """
 
 def main():
+    print "Welcome!"
+    print ""
+    print "--------------------"
+    print "1: Calculate using Ohm's Law"
+    print "2: Calculate a resistor's value"
+    print "--------------------"
+    print ""
+    choice = int(raw_input("Enter the value for which function you wish to perform: "))
+    if choice == 1:
+        calculateOhms()
+    elif choice == 2:
+        calculateResist()
+
+def calculateOhms():
     print("Welcome to EE Helper")
-    print("Currently, I'm able to calculate Ohm's law!")
+    print("Let's calculate some values with Ohm's Law!")
     units = ["Volts", "Amps", "Ohms"]
 
     v = raw_input("Enter the voltage: ")
@@ -97,6 +111,95 @@ def errorHandler(e):
         main()
     else:
         exit()
+
+def calculateResist():
+    print("Calculating Resistance!")
+    bands = []
+    for i in range(4):
+        print "Band " + str(i+1)
+        color = raw_input("Enter the color of the band: ")
+        bands.append(getColorVals(color, i+1))
+    bandValue = 0
+    bandValue = str(bands[0])+str(bands[1])
+    bandValue = int(bandValue)*float(bands[2])
+    print("--------------------")
+    print str(bandValue) + " Ohms"
+    print "Tolerance: " + str(bands[3])+"%"
+    print("--------------------")
+
+    # Ask to restart
+    rerun = raw_input("Would you like to do another? (Y/N) ")
+    while rerun.upper() != "Y" and rerun.upper() != "N":
+        rerun = raw_input("Would you like to do another? (Y/N) ")
+    if rerun.upper() == "Y":
+        calculateResist()
+    else:
+        exit()
+
+
+def getColorVals(color, bandNum):
+    if bandNum == 1 or bandNum == 2:
+        if color.lower() == "black":
+            return 0
+        elif color.lower() == "brown":
+            return 1
+        elif color.lower() == "red":
+            return 2
+        elif color.lower() == "orange":
+            return 3
+        elif color.lower() == "yellow":
+            return 4
+        elif color.lower() == "green":
+            return 5
+        elif color.lower() == "blue":
+            return 6
+        elif color.lower() == "purple" or color.lower() == "violet":
+            return 7
+        elif color.lower() == "grey":
+            return 8
+        elif color.lower() == "white":
+            return 9
+    elif bandNum == 3:
+        if color.lower() == "black":
+            return 1
+        elif color.lower() == "brown":
+            return 10
+        elif color.lower() == "red":
+            return 100
+        elif color.lower() == "orange":
+            return 1000
+        elif color.lower() == "yellow":
+            return 10000
+        elif color.lower() == "green":
+            return 100000
+        elif color.lower() == "blue":
+            return 1000000
+        elif color.lower() == "purple" or color.lower() == "violet":
+            return 10000000
+        elif color.lower() == "gold":
+            return 0.1
+        elif color.lower() == "silver":
+            return 0.01
+    elif bandNum == 4:
+        if color.lower() == "brown":
+            return 1
+        elif color.lower() == "red":
+            return 2
+        elif color.lower() == "green":
+            return 0.5
+        elif color.lower() == "blue":
+            return 0.25
+        elif color.lower() == "purple" or color.lower() == "violet":
+            return 0.10
+        elif color.lower() == "grey":
+            return 0.05
+        elif color.lower() == "gold":
+            return 5
+        elif color.lower() == "silver":
+            return 10
+
+
+
 
 
 main()
